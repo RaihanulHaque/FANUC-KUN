b1545b816fed0f54efdb66e87535f80ff469f069
Rahi <raihanulhaque007@gmail.com>
2022-05-20 09:35:50 +0600

    Django setup completed for image transfer

---
+++ home/admin.py
@@ -0,0 +1,9 @@
+import imp
+from django.contrib import admin
+from .models import Image
+# Register your models here.
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['image']
